2944651adfb33ed80cbb3da96df580bb4b3b1dd0
SynedraAcus <alexeymorozov1991@gmail.com>
2019-02-06 21:12:41 +0800

    Changes to returning focus

--- listeners.py
+++ listeners.py
@@ -28,10 +28,11 @@ class ScrollListener(Listener):
     `BearEvent(event_type='brut_temporary_focus', event_value=entity_id)`:
     focus on a given entity so that a screen will track its movements; when it
     is destroyed (as evidenced by 'ecs_destroy' event), returns focus to
-    whatever it was focused on before. Does not support recursive re-focusing;
+    whatever it was focused on before and scroll back to wherever the screen was
+    before initiating temporary focus. Does not support recursive re-focusing;
     if temporary focus is called while already in temporary focusing mode, the
     screen will track a new target, but upon entity destruction it will return
-    to the original target, not to the previous temporary one.
+    to the original target and position, not to the previous temporary one.
     
     `BearEvent(event_type='ecs_destroy')` and `BearEvent(event_type='ecs_move')`
      are necessary for this Listener to work, but should not be used to interact
@@ -48,6 +49,7 @@ class ScrollListener(Listener):
         self.layout = layout
         self.distance = distance
         self.old_target = None
+        self.old_pos = None
         
     @property
     def target_entity(self):
@@ -68,12 +70,18 @@ class ScrollListener(Listener):
             if not self.old_target:
                 self.old_target = self.target_entity
             self.target_entity = event.event_value
-        elif event.event_type == 'ecs_destroy' and self.old_target is not None:
+            self.old_pos = [x for x in self.layout.view_pos]
+        elif event.event_type == 'ecs_destroy' and  \
+                    event.event_value == self.target_entity \
+                    and self.old_target is not None:
+            # Return focus to the old target and scroll back to it
             self.target_entity = self.old_target
             self.old_target = None
+            return BearEvent(event_type='ecs_scroll_to',
+                             event_value=(self.old_pos))
         elif event.event_type == 'ecs_move' and event.event_value[0] == self.target_entity:
             # There is no support for y-scrolling because this is a
-            # side-scrolling (sic) beat'em'up. If this piece of code makes it to
+            # side-scrolling (sic) beat'em'up. If this piece of code makes idt to
             # bear_hug or other project, it would be trivial to add by replacing
             # some [0]s with [1]s and x's with y's
             x = event.event_value[1]
